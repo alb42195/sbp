b5c8e37c8f413be069889e54fefdd1d059fb114e
alb <albert@hayr.at>
2014-02-27 14:41:08 +0100

    implemented class cluster,cnode

--- sbp.py
+++ sbp.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
-import struct, time, socket, queue, threading
+import struct, time, socket, queue, threading, json
 
 class heartbeat():
-  def __init__(self, ClusterID, NodeID):
-    self.ClusterID = ClusterID
-    self.NodeID = NodeID
+  def __init__(self):
+    pass
 
-  def pack_hb(self, src_nID, des_nID, lID, seqnum, acknum):
-    data_pack = struct.pack('HHHHIId', self.ClusterID, self.NodeID, des_nID, lID, seqnum, acknum, time.time()) 
+  def pack_hb(self, src_nID, des_nID, lID, seqnum, acknum, ClusterID, NodeID):
+    data_pack = struct.pack('HHHHIId', ClusterID, NodeID, des_nID, lID, seqnum, acknum, time.time()) 
     return data_pack
 
   def unpack_hb(self, data_pack):
@@ -85,29 +84,29 @@ class hb_rx(heartbeat,threading.Thread):
       
 
 class hb_tx(heartbeat,threading.Thread):
-  def __init__(self,txq,ClusterID,NodeID):
+  #def __init__(self,txq,ClusterID,NodeID):
+  def __init__(self,txq):
     threading.Thread.__init__(self)
     self.daemon = True
     self.sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-    self.ClusterID = ClusterID
-    self.NodeID = NodeID
+    #self.ClusterID = ClusterID
+    #self.NodeID = NodeID
     self.txq = txq
 
   def run(self):
     while True:
       x = self.txq.get()
-      self.tx(x[0],x[1],x[2],x[3],x[4],x[5])
+      self.tx(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7])
       self.txq.task_done()
 
-  def tx(self,src_ip,host,dst_nID,lID,seq,ack):
+  def tx(self,src_ip,host,dst_nID,lID,seq,ack,ClusterID,NodeID):
     dst_ip = socket.gethostbyname(host)
-    src_ip = src_ip
     ip_header = self.pack_ip(src_ip,dst_ip)
     cksum = 0
-    icmp_header = struct.pack("bbHHh", 8, 0, cksum, self.ClusterID, 1)
-    data = self.pack_hb(self.NodeID,dst_nID,lID,seq,ack)
+    icmp_header = struct.pack("bbHHh", 8, 0, cksum, ClusterID, 1)
+    data = self.pack_hb(NodeID,dst_nID,lID,seq,ack, ClusterID, NodeID)
     cksum = socket.htons(self.checksum(icmp_header + data))
-    icmp_header = struct.pack("bbHHh", 8, 0, cksum, self.ClusterID, 1)
+    icmp_header = struct.pack("bbHHh", 8, 0, cksum, ClusterID, 1)
     pkt = ip_header + icmp_header + data
     self.sock.sendto(pkt, (dst_ip, 1))
 
@@ -122,7 +121,7 @@ class send_msg(threading.Thread):
     while True:
       time.sleep(self.link.time)
       self.link.seqnum += 1
-      self.link.txq.put([self.link.src_ip, self.link.dst_ip,self.link.dst_nID,self.link.lID,self.link.seqnum,self.link.acknum])
+      self.link.cluster.txq.put([self.link.src_ip, self.link.dst_ip,self.link.dst_nID,self.link.lID,self.link.seqnum,self.link.acknum,self.link.cluster.ID,self.link.cluster.NodeID])
       
 
 class get_msg(threading.Thread):
@@ -138,9 +137,8 @@ class get_msg(threading.Thread):
       print(pkt)
       self.link.rxq.task_done()
 
-class icmp_link():
-  def __init__(self,txq,time,src_ip,dst_ip,dst_nID,lID):
-    self.txq = txq
+class ICMP_link():
+  def __init__(self,cluster,time,src_ip,dst_ip,dst_nID,lID):
     self.time = time
     self.src_ip = src_ip
     self.dst_ip = dst_ip
@@ -151,10 +149,55 @@ class icmp_link():
     self.tx = send_msg(self)
     self.rxq = queue.Queue() 
     self.rx = get_msg(self)
+    self.cluster = cluster
 
   def start(self):
     self.tx.start()
     self.rx.start()   
-  
 
+class cluster():
+  def __init__(self,config,rxq,txq):
+    self.config = config
+    self.rxq = rxq
+    self.txq = txq
+    self.ID = self.config["ID"]
+    self.hostname = socket.gethostname()
+    self.NodeID = "AAA"
+    self.Nodes = {}
 
+  def create_nodes(self):
+    for i in self.config["Members"]:
+      if i['Hostname'] == self.hostname:
+        self.NodeID = i['ID']
+      else:
+        self.Nodes[i['ID']] = cnodes(self,i['ID'],i['Hostname'])
+
+  def create_all_icmp_links(self):
+    for x,z in self.Nodes.items():
+      z.create_links()
+
+class cnodes():
+  def __init__(self,cluster,dst_nID,hostname):
+    self.cluster = cluster
+    self.ID = dst_nID
+    self.hostname = hostname
+    self.icmp_links = {}
+
+  def create_links(self):
+    for i in self.cluster.config["ICMP_links"]:
+      own_link = False
+      if len(i['Nodes']) != 2:
+        print("ERROR")
+      for x in i['Nodes']:
+        if x['NodeID'] == self.cluster.NodeID:
+          own_link = True
+        else:
+          dst_nID = x['NodeID']
+          ip = x['IP']
+      if own_link:
+        self.icmp_links[i['ID']] = ICMP_link(self.cluster,i['interval'],ip,i['ICMPIP'],dst_nID,i['ID'])
+
+  def start_all_links(self):
+    for i in self.icmp_links:
+      print("AAAA")
+      self.icmp_links[i].start()
--- sbpd.py
+++ sbpd.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
 
-import threading, queue, sbp, time
+import threading, queue, sbp, time, json
 
 rxq = queue.Queue()
 txq = queue.Queue()
 
+with open('sbp.conf') as f:
+  config = json.load(f)
 
-l = {}
-l[1] = sbp.icmp_link(txq,2,'192.168.122.12','192.168.122.13',12,1)
-l[1].start()
-tx_t = sbp.hb_tx(txq, 1000,11)
+tx_t = sbp.hb_tx(txq)
 tx_t.start()
-rx_t = sbp.hb_rx(l,'',1,1000,11)
-rx_t.start()
+x = sbp.cluster(config['Cluster'][0],rxq,txq)
+x.create_nodes()
+x.create_all_icmp_links()
+x.Nodes[2].start_all_links()
+
+
+#l = {}
+#l[1] = sbp.icmp_link(txq,2,'192.168.122.12','192.168.122.13',12,1)
+#l[1].start()
+#rx_t = sbp.hb_rx(l,'',1,1000,11)
+#rx_t.start()
 
 
 
